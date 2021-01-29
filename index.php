d0e023a7fcd168372828c7358f10da11c1c27fe8
astakova <shesna2000@mail.ru>
2021-01-29 15:27:45 +0300

    l3

--- index.php
+++ index.php
@@ -1,47 +1,26 @@
 <?php
-class A
-{
-	public function solve($a, $b)
-	{
-		if ($a==0)
-		{
-			return NULL;
-		}
-		else
-		{
-			return $this->x=-($b/$a);
-		}
-	}
-	protected $x;
-}
-class B extends A
-{
-	protected function discriminant($a, $b, $c)
-	{
-		return $b**2-4*$a*$c;
-	}
-	public function quadratic_solve($a, $b, $c)
-	{
-		if($a==0)
-		{
-			return($this->solve($a, $b));
-		}
-		$d=$this->discriminant($a, $b, $c);
-		if($d<0)
-		{
-			return NULL;
-		}
-		if($d==0)
-		{
-			return $this->x=(-($b)+sqrt($d));
-		}
-		return $this->x=array((-($b)+sqrt($d))/2*$a,(-($b)-sqrt($d))/2*$a);
-		}
-	}
-$a=new A();
-$b=new B();
-$b->quadratic_solve(2, 1, 2);
-$a->solve(2, 3);
-var_dump($a, $b);
+use shesna\ShesnaException;
+use shesna\Sqrt;
+use shesna\MyLog;
+
+include "core/EquationInterface.php";
+include "core/LogInterface.php";
+include "core/LogAbstract.php";
+include "shesna/MyLog.php";
+include "shesna/Line.php";
+include "shesna/Sqrt.php";
+
+
+ini_set("display_errors", 1);
+error_reporting(-1);
+
+$a = MyLog::Instance();
+$b = MyLog::Instance();
+
+$a->log('1');
+$a->log('2');
+$a->log('3');
+$a->log('4');
+$b->write();
 ?>
-		
+
